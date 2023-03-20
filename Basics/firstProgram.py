54ad28ce6c2b0cd10dc6abac2c121e3f4921ee5d
Thato/ <t.j.mahlatji@gmail.com>
2023-03-20 22:16:47 +0200

    Create firstProgram.py

---
+++ Basics/firstProgram.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 21:30:45 2023
+
+@author: Thati
+"""
+print("--------- Intro ------------")
+# First line of code
+print("Hello World")
+
+print("My name is Thato")
+
+"""
+Text Types: 
+    String
+
+Numeric Types:
+    integers: int
+    floats: float
+    complex
+"""
+print("--------- String------------")
+name = 'Thato'
+surname = "Mahlatji"
+
+print(name+ ' ' + surname)
+
+print("----------------------------")
+print("--------- Numeric------------")
+print("--------- Integers------------")
+
+a = 5
+print("Integer = ", a)
+
+print("--------- float------------")
+b = 5.0
+print("float = ",b)
+
+print("--------- Complex------------")
+
+z = 1j
+print("complex = ", z)
+
+print("--------- List ------------")
+l = [1,2,3,4,5]
+k = ['apples','bananas','mango','pineapple']
+
+print('count = ',l)
+print("fruits = ", k)
+
+
+print("--------- Boolean------------")
+
+boolean = True
+
+print("Bool = ", boolean)
+
